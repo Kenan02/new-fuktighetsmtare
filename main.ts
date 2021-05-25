21bc277de46f6f5665bdc9ac79af6fe295b376c8
Kenan02 <61836371+Kenan02@users.noreply.github.com>
2021-05-25 14:59:57 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+OLED.init(128, 64)
 basic.forever(function () {
-	
+    OLED.writeNumNewLine(tinkercademy.MoistureSensor(AnalogPin.P0))
+    if (tinkercademy.MoistureSensor(AnalogPin.P0) < 50) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(1000)
+    }
 })
